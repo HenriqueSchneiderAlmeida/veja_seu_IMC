a5c3c5d67e8bd065b7301efa8488ddda8c19a92a
Henrique <Henr.s.almeida@gmail.com>
2023-07-08 22:33:21 -0300

    ajuste rodape

--- src/components/Formulario/rodape.jsx
+++ src/components/Formulario/rodape.jsx
@@ -3,9 +3,9 @@ import styles from './formularia.module.css';
 const Rodape = ({}) => {
     return ( 
         <footer className={styles.footer}>
-            <h3>
-                Quer um site? -- Henrique Schneider / (11) 97409-4438
-            </h3>
+            <h4>
+                Quer um site? - Henrique Schneider (11) 97409-4438
+            </h4>
         </footer>
     )
 }
