4ecade6c6df654e7dfe581b2ee7b554d8aebde13
Henrique <Henr.s.almeida@gmail.com>
2023-07-08 22:16:17 -0300

    Incluindo o rodapé

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import Formulario from "./components/Formulario";
-  
+import Rodape from "./components/Formulario/rodape";  
+
 function App() { 
   return (   
         <div>
           <Formulario></Formulario>   
+          <Rodape></Rodape>
         </div>
+
   )
 }
 export default App
---
+++ src/components/Formulario/rodape.jsx
@@ -0,0 +1,12 @@
+import styles from './formularia.module.css';
+
+const Rodape = ({}) => {
+    return ( 
+        <footer className={styles.footer}>
+            <h3>
+                Quer um site? -- Henrique Schneider / (11) 97409-4438
+            </h3>
+        </footer>
+    )
+}
+export default Rodape;
